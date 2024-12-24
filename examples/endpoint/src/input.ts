2cf167c0ed79eb4356516d860b67d5e8c1509741
Jonathan Stanley <jonathan@bristleconeweb.com>
2024-12-23 21:35:12 -0500

    chore: drop headers
    not accessible at endpoint

--- examples/endpoint/src/input.ts
+++ examples/endpoint/src/input.ts
@@ -21,53 +21,23 @@ const themeSchema = v.object({
   quantity: natNum,
 });
 
-/** data extracted from request headers */
-const headerSchema = v.object({
-  productId: natNum,
-  variantId: natNum,
-});
-
 /** the data expected in the form payload */
-const formDataSchema = v.object({
+const inputSchema = v.object({
   ...themeSchema.entries,
   lengthInches: v.pipe(v.number(), v.minValue(0), v.finite()),
   widthInches: v.pipe(v.number(), v.minValue(0), v.finite()),
   heightInches: v.pipe(v.number(), v.minValue(0), v.finite()),
 });
-
-/** the combined data expected for calculations */
-const inputSchema = v.object({
-  ...formDataSchema.entries,
-  ...headerSchema.entries,
-});
 export type Input = v.InferOutput<typeof inputSchema>;
-
-/** extracts the data we expect from the headers */
-const parseHeaders = (req: Request) => {
-  const productId = req.headers.get("product-id");
-  const variantId = req.headers.get("variant-id");
-  return {
-    productId: productId ? +productId : null,
-    variantId: variantId ? +variantId : null,
-  };
-};
-
-/**
- * extracts the data we expect from the FormData payload
- *
- * `decode` converts FormData to an object with the prescribed primitives
- * https://github.com/fabian-hiller/decode-formdata
- */
-const parseFormData = (formData: FormData) => {
-  return decode(formData, {
-    numbers: ["quantity", "lengthInches", "widthInches", "heightInches"],
-  });
-};
 
 /** Translate formData into expected schema or provide error */
 export const parseRequest = async (req: Request) => {
-  return v.safeParse(inputSchema, {
-    ...parseFormData(await req.formData()),
-    ...parseHeaders(req),
+  /**
+   * Converts FormData to an object with the prescribed primitives
+   * https://github.com/fabian-hiller/decode-formdata
+   */
+  const formValues = decode(await req.formData(), {
+    numbers: ["quantity", "lengthInches", "widthInches", "heightInches"],
   });
+  return v.safeParse(inputSchema, formValues);
 };
